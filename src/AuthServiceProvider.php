519cbd59155a6f82c58e2212a64f1c99b67fae88
gh-huang <gh_huang@outlook.com>
2016-09-05 16:52:05 +0800

    provider defer

--- src/AuthServiceProvider.php
+++ src/AuthServiceProvider.php
@@ -7,6 +7,13 @@ use Illuminate\Auth\AuthServiceProvider as OriginalAuthServiceProvider;
 class AuthServiceProvider extends OriginalAuthServiceProvider
 {
 	/**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+	/**
 	* Register bindings in the container.
 	* @return void
 	*/
